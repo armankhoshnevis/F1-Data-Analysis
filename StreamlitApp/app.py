76a9dd8c969a79540153e2717ca70a14d8d75e84
Arman Khoshnevis <khoshne1@msu.edu>
2024-10-11 16:59:59 -0400

    Adjust the Python Path

--- StreamlitApp/app.py
+++ StreamlitApp/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.race_schedule_vis import (
     f1_circuit_world_map, 
